aaea2e2da34b51e625dece7a2f067285d9a11d51
Donatas <jurkeviciusdonatas@gmail.com>
2022-01-14 17:32:53 +0200

    Add onCreate, onStart, onResume on MainActivity

--- app/src/main/java/lt/vcs/donatasj_homework/MainActivity.java
+++ app/src/main/java/lt/vcs/donatasj_homework/MainActivity.java
@@ -1,14 +1,43 @@
 package lt.vcs.donatasj_homework;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "log_main_activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        printStatus("--------");
+        printStatus("onCreate");
+        setUP();
+    }
+    private void setUP(){
         setContentView(R.layout.activity_main);
     }
+
+
+    private void printStatus(String statusName) {
+        Log.i(LOG_TAG, "1st activity " + statusName);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        printStatus("--------");
+        printStatus("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        printStatus("--------");
+        printStatus("onResume");
+    }
+
 }
